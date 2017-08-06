30f892bab9abefb5abb0a6134a68316a8b2c3656
Muhammad Azeem <m7azeem@gmail.com>
2017-08-06 20:24:26 +0400

    Add code to generate the Genesis Block

--- main.js
+++ main.js
@@ -21,3 +21,9 @@ var generateNextBlock = (blockData) => {
     var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
 };
 
+var getGenesisBlock = () => {
+    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+};
+
+var blockchain = [getGenesisBlock()];
+
