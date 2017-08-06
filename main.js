cf7f2bacb7e389ce299af2c76d266f0d8acef135
Muhammad Azeem <m7azeem@gmail.com>
2017-08-06 20:34:02 +0400

    Add code to verify new block(s)

--- main.js
+++ main.js
@@ -21,9 +21,24 @@ var generateNextBlock = (blockData) => {
     var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash);
 };
 
+//first block in blockchain
 var getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
+//initialize the blockchain
 var blockchain = [getGenesisBlock()];
 
+var isValidNewBlock = (newBlock, previousBlock) => {
+    if (previousBlock.index + 1 !== newBlock.index) {
+        console.log('invalid index');
+        return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+        console.log('invalid previousHash');
+        return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+        console.log('invalid hashL ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
+        return false;
+    }
+    return true;
+};
