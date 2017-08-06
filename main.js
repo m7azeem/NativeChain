1ad2988cc109c5f770c88d3dd874175e02540c79
Muhammad Azeem <m7azeem@gmail.com>
2017-08-06 21:11:29 +0400

    Add user connectivity

--- main.js
+++ main.js
@@ -54,3 +54,29 @@ var replaceChain = (newBlocks) => {
     }
 };
 
+var initHttpServer = () => {
+    var app = express();
+    app.use(bodyParser.json());
+
+    app.get('/blocks', (req, res) => res.send(JSON.stringifyblockchain()));
+
+    app.post('/mineBlock', (req, res) => {
+        var newBlock = generateNextBlock(req.body.data);
+        addBlock(newBlock);
+        broadcast(responseLatestMsg());
+        console.log('block added: ' + JSON.stringify(newBlock));
+        res.send();
+    });
+
+    app.get('/peers', (req, res) => {
+        res.send(sockets.map(s=>s.socket.remoteAddress + ':' + s.socket.remotePort)); 
+    });
+    app.post('/addPeer', (req, res) => ){
+        connectToPeers([req.body.peer]);
+        res.send();
+    });
+
+    app.listen(http_port, () => console.log('Listening http on port: ') + http_port));
+};
+
+
